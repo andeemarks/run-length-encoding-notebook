be97d4e5f831b3f3f1a824903fa1870ef9f46cfd
Andy Marks <vampwillow@gmail.com>
2021-07-25 10:21:19 +1000

    Sync with notebook code

--- src/run_length_encoding_notebook/core.clj
+++ src/run_length_encoding_notebook/core.clj
@@ -1,7 +1,26 @@
-(ns run-length-encoding-notebook.core
-  (:gen-class))
+(ns run-length-encoding-notebook.core)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn run-length-encode
+  [s]
+  (let [runs (partition-by identity s)] ;; group by value
+    (apply str ;; combine counts and lengths into single string
+           (remove #(= % 1) ;; remove any single length run counts
+                   (interleave ;; combine the corresponding count and value
+                    (map count runs) ;; count the length of each run
+                    (map first runs)))))) ;; isolate the value of each run
+
+(defn- component-to-run [component]
+  (let [length (Integer/parseInt (first (re-seq #"\d+" component)))
+        value (first (re-seq #"[^\d]+" component))
+        run (apply str (repeat length (get value 0)))]
+    (if (<= 1 (count value))
+      (str run (apply str (rest value)))
+      run)))
+
+(defn run-length-decode
+  "decodes a run-length-encoded string"
+  [cipher-text]
+  (let [components (re-seq #"\d+[^\d]+" cipher-text)]
+    (if (nil? components)
+      cipher-text
+      (apply str (map #(component-to-run %) components)))))
